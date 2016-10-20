4c6cc89789b1bedfab90f597da21c1ead8e0fe0d
ishakdolek <pro-com65@hotmail.com>
2016-10-20 17:00:38 +0300

    Add project files.

---
+++ src/WebApplication1/Models/Gmail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public  static  class  Gmail
+    {
+        
+    }
+}
---
+++ src/WebApplication1/Yonetim/Controller/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Yonetim.Controller
+{
+    public class AdminController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Widgets()
+        {
+            return View();
+        }
+
+        public IActionResult Charts()
+        {
+            return View();
+        }
+
+        public IActionResult Forms()
+        {
+            return View();
+        }
+
+        public IActionResult Panels()
+        {
+            return View();
+        }
+
+        public IActionResult Icons()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
